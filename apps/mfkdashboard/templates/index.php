7ea0f02799d9f24e7c777930a3a69ce65a0bdd26
Toseef Ahmed <peekinternational@gmail.com>
2024-10-31 08:35:52 +0000

    Dashboard Layout - Index

--- apps/mfkdashboard/templates/index.php
+++ apps/mfkdashboard/templates/index.php
@@ -1,3 +1,17 @@
+<?php ?>
+//header('Location: /hr');
+//exit;
+
 <?php
-header('Location: /hr');
-exit;
+$page_title = 'HR';
+include_once(__DIR__ . '/includes/header.php');
+?>
+<!-- Main Content -->
+<div class="container-fluid p-4 main-content-area">
+    <h2>Main Content</h2>
+    <p>This is where the main content will appear.</p>
+</div>
+
+<?php
+include_once(__DIR__ . '/includes/footer.php');
+?>
